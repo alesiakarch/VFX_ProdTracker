d0d02a285807f5b4f8d7b28863c334784b034118
alesiakarch <120693642+alesiakarch@users.noreply.github.com>
2025-06-29 20:45:44 +0100

    set up SQLite database to store projects

--- tracktor_server/main.py
+++ tracktor_server/main.py
@@ -1,9 +1,27 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import sqlite3
 
 app = Flask(__name__)
-cors = CORS(app, origins='*') # specify origins
+cors = CORS(app, resources={r"/*": {"origins": "*"}}) # specify origins
 
+def get_db():
+    connection = sqlite3.connect("projects.db")
+    connection.row_factory = sqlite3.Row
+    return connection
+
+@app.route("/init", methods = ['GET'])
+def init_db():
+    conn = get_db()
+    conn.execute("""
+                    CREATE TABLE IF NOT EXISTS projects(
+                 id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                 name TEXT NOT NULL
+                 )
+                 """)
+    conn.commit()
+    conn.close()
+    return jsonify({"message" : "Database init complete"})
 projects = []
 
 @app.route("/api/users", methods =['GET'])
@@ -20,7 +38,10 @@ def users():
 
 @app.route("/api/projects", methods=['GET'])
 def existing_projects():
-    return jsonify(projects)
+    conn = get_db()
+    rows = conn.execute("SELECT * FROM projects").fetchall()
+    conn.close()
+    return jsonify([dict(row) for row in rows])
 
 @app.route("/api/projects", methods=['POST'])
 def create_project():
@@ -28,13 +49,24 @@ def create_project():
     if "name" not in data:
         return jsonify({"error":"Missing the project's name"}), 400
     
-    new_project = {"id": len(projects) + 1, "name" : data["name"]}
+    name = data.get("name")
+    conn = get_db()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO projects(name) VALUES(?)", (name,))
+    conn.commit()
+    new_id = cursor.lastrowid
+    conn.close()
 
-    projects.append(new_project)
+
+    
+    new_project = {"id": new_id, "name" : name}
+
     print("Current projects:", projects)
     return jsonify(new_project), 201
 
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        init_db()
     app.run(debug=True, port=8080)
